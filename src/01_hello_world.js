169aaaf4d6bcaa15b5cb4cdee10973acfb7864fc
jy4education-gif <jy4education@gmail.com>
2026-01-09 10:40:08 +0000

    Aktualisiere Hauptdatei in package.json und füge Konsolenausgaben in 01_hello_world.js hinzu

--- src/01_hello_world.js
+++ src/01_hello_world.js
@@ -1,1 +1,5 @@
-
+ console.log("Hello, World!");
+ console.log(new Date().toLocaleString());
+ console.log(`Hello, World it is ${new Date().toLocaleString()}`);
+ console.log(new Date().toISOString());
+ console.log("Hello, World", new Date().toLocaleTimeString());
