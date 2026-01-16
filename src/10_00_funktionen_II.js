9c6cfe970a9970a3bead4355916a028d7e5e850c
jy4education-gif <jy4education@gmail.com>
2026-01-16 11:35:31 +0000

    +

--- src/10_00_funktionen_II.js
+++ src/10_00_funktionen_II.js
@@ -6,38 +6,13 @@
 
 1. Dateneingabe + -überprüfung :  
 2. Auswahl Rechenart : 
-3. Fkt. Grundrechenarten : 
+3. Fkt. Grundrechenarten : check!
 4. Ausgabe in Konsole : check!
 */
 
+const ERROR_STR_DIV = "Teilen durch 0 nicht möglich!";
 
-// module: multiplication a * b |  test:
-output(mul(3,2));
-output(mul(3,-2));
-output(mul(3,0));
-function mul(a,b) {
-	return `Multiplikation: ${a} * ${b} = ${a*b}`;
-}
-
-// module: subion a - b |  test:
-output(sub(3,2));
-output(sub(3,-2));
-output(sub(3,0));
-output(sub(0,2));
-function sub(a,b) {
-	return `Subtraktion: ${a} - ${b} = ${a - b}`;
-}
-
-// module: division a / b |  test:
-output(div(3,2));
-output(div(3,-2));
-output(div(3,0));
-output(div(0,2));
-function div(a,b) {
-	return `Division: ${a} / ${b} = ${a/b}`;
-}
-
-//** WIE sollen die Aufgaben gelöst werden? **/
+/** WIE sollen die Aufgaben gelöst werden? */
 
 // module: division a / b |  test:
 // output(divide(4,2));
@@ -46,16 +21,51 @@ function div(a,b) {
 // output(divide(0,2));
 // output(divide(3,0));
 // output(divide(0,0));
+function divide(a,b) {
 
+	// 1. Iteration
+	// if (b==0) {
+	// 	return "Teilen durch 0 nicht möglich!";
+	// } else {
+	// 	return a/b;
+	// }
 
+	// 2. Iteration
+
+	// Ausnahme
+	if (b==0) {
+		return ERROR_STR_DIV;
+	} 
+	
+	// Regel
+	return a/b;
+	
+}
+
+// module: multiplication a * b |  test:
+// output(multiply(3,2));
+// output(multiply(3,-2));
+// output(multiply(3,0));
+function multiply(a,b) {
+	return  a*b;
+}
+
+// module: subtraction a - b |  test:
+// output(subtract(3,2));
+// output(subtract(3,-2));
+// output(subtract(3,0));
+// output(subtract(0,2));
+function subtract(a,b) {
+	return a-b;
+}
 
 // module: addition a + b |  test:
-output(add(2,2));
-output(add(2,-2));
-output(add(2,0));
-function add(a, b) {
-	return `Addition: ${a} + ${b} = ${a + b}`;
-}	
+// output(add(2,2));
+// output(add(2,-2));
+// output(add(2,0));
+function add(a,b) {
+	return a+b;
+}
 
 // module: output | test:
 // output("hello");
