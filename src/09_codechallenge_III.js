8f2a00598faaea4248437a333b5cc9411c13e00f
jy4education-gif <jy4education@gmail.com>
2026-01-16 11:50:50 +0000

    add link repo

--- src/09_codechallenge_III.js
+++ src/09_codechallenge_III.js
@@ -7,5 +7,6 @@
 // simuliert. Orientieren Sie sich dabei am Struktogramm in der Datei
 // bestellung.json  - Datei bitte in den online-Editor STRUCTOG laden
 // https://ddi.education/struktog/
+// https://github.com/archive-Basic-P-25-09/02a_Bestellung
 
 
