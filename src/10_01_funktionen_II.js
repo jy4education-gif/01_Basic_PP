372e24ed5690701f4946cbe8541f45962424f571
jy4education-gif <jy4education@gmail.com>
2026-01-16 11:36:46 +0000

    +

--- src/10_01_funktionen_II.js
+++ src/10_01_funktionen_II.js
@@ -99,7 +99,7 @@ const prompt = require('prompt-sync')({sigint: true});
 const add = (a, b) => a + b;
 const sub = (a, b) => a - b;
 const mul = (a, b) => a * b;
-const div = (a, b) => b === 0 ? "Fehler: Division durch Null" : a / b;
+const div = (a, b) => b == 0 ? "Fehler: Division durch Null" : a / b;
 
 // 2. Lookup-Objekt (Mapping)
 const operations = {
