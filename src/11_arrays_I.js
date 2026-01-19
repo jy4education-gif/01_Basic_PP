8dfcc8fd2f4b8e7eb3fa5737a4e60d68e36ad618
jy4education_git <jy4education@gmail.com>
2026-01-19 11:32:45 +0100

    + Wert eines Arrays zu einem Index und den Index selber ausgeben

--- src/11_arrays_I.js
+++ src/11_arrays_I.js
@@ -6,8 +6,15 @@
  * 
  */
 
-
-
+/*** 02. Theorie: Array */
+let arr;
+// arr = new Array();
+arr = []; // leeres Array
+arr = [2,11,7,8,10,20]; // Array mit Zahlen
+arr[0] = 99; // Wert an Index 0 ändern
+console.log(arr);
+console.log("Länge des Arrays:", arr.length); // Länge des Arrays
+console.log(`Wert an der Stelle ${arr.indexOf(arr[2])} ist ${arr[2]}`); // Wert an Index 2 ausgeben
 /**
  *   PRAXIS
  * 
